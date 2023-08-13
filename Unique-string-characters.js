29ce37cde446cb20f9be61ba52a891589372a534
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-12 21:01:52 -0700

    Update Unique-string-characters.js

--- Unique-string-characters.js
+++ Unique-string-characters.js
@@ -19,3 +19,18 @@ function solve(a,b){
  };
 
  // or
+
+ function solve(a,b){
+  let arr = [];
+  for (let i = 0; i < a.length; i++){
+      if (!(b.includes(a[i]))){
+         arr.push(a[i])
+      }
+  }
+  for (let i = 0; i < b.length; i++){
+      if (!(a.includes(b[i]))){
+          arr.push(b[i])
+      }
+  }
+  return arr.join("");
+}
